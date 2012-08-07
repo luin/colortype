ab13f2b986e6a9b07f7f45c52af46cad0c93bc1b
Zihua Li <i@zihua.li>
2012-08-07 15:50:15 +0800

    show custom footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
       </div>
-      <div style="clear: both"></div>
     </div>
     <?php wp_footer() ?>
+    <?php $options = get_option('colortype_theme_options'); ?>
+    <?php if ($options['footer']) echo $options['footer']; ?>
   </body>
 </html>
